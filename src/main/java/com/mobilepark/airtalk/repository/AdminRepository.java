dd072a6e16b7dadfc55db090c53bf024af5ff65c
sss424 <sss424@i-mobilepark.com>
2020-10-20 09:18:06 +0900

    오류 수정

--- src/main/java/com/mobilepark/airtalk/repository/AdminRepository.java
+++ src/main/java/com/mobilepark/airtalk/repository/AdminRepository.java
@@ -8,10 +8,5 @@ import java.util.List;
 import com.mobilepark.airtalk.data.Admin;
 
 @Repository
-public class AdminRepository extends JpaRepository<Admin, String>{
-
-	public List<Admin> findAll() {
-		return null;
-	}
-    
+public interface AdminRepository extends JpaRepository<Admin, String> {
 }
