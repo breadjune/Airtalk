a7a7e3a554773303a9cd1c6ec79ab3b01387f66c
sss424 <sss424@i-mobilepark.com>
2020-10-20 11:51:11 +0900

    오타 및 오류 수정

--- src/main/java/com/mobilepark/airtalk/repository/AdminRepository.java
+++ src/main/java/com/mobilepark/airtalk/repository/AdminRepository.java
@@ -3,8 +3,6 @@ package com.mobilepark.airtalk.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 import com.mobilepark.airtalk.data.Admin;
 
 @Repository
