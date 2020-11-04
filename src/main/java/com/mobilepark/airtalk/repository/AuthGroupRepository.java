147b30dd2cd578144c3a4a7800f7d91ff05eccf7
gksjs3468 <gksjs3468@uangel.com>
2020-11-04 09:08:30 +0900

    [권한 관리] DB 이름 교체 및 수정

--- src/main/java/com/mobilepark/airtalk/repository/AuthGroupRepository.java
+++ src/main/java/com/mobilepark/airtalk/repository/AuthGroupRepository.java
@@ -1,13 +1,13 @@
 package com.mobilepark.airtalk.repository;
 
-import com.mobilepark.airtalk.data.AuthGroup;
+import com.mobilepark.airtalk.data.AdminGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuthGroupRepository extends JpaRepository<AuthGroup, Integer> {
+public interface AuthGroupRepository extends JpaRepository<AdminGroup, Integer> {
 
 //     @Query(value="SELECT NAME FROM TBL_ADMIN_GROUP WHERE ADMIN_GROUP_SEQ = :admin_group_seq LIMIT 1", nativeQuery=true)
 //     public AdminGroup nativeFindByAdminGroupSeq(@Param("admin_group_seq") Integer admin_group_seq);
