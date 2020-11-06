10c6c5c14bf83600c5f8a7131516ac99a19c4659
youngjun93 <youngjun93@i-mobilepark.com>
2020-11-06 17:39:05 +0900

    유효성 검증 수정

--- src/main/java/com/mobilepark/airtalk/interceptor/AuthenticationInterceptor.java
+++ src/main/java/com/mobilepark/airtalk/interceptor/AuthenticationInterceptor.java
@@ -6,6 +6,7 @@ import com.mobilepark.airtalk.service.LoginService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
@@ -20,29 +21,34 @@ public class AuthenticationInterceptor implements HandlerInterceptor {
     // @Autowired
     // public TokenProvider tp;
 
+    @Value("${spring.jwt.token.secret-key}") String secretKey;
+
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 
         boolean expire = false;
-
+        
         TokenProvider tp = new TokenProvider();
 
         Cookie[] cookies = request.getCookies();
         if(cookies != null) {
             for(Cookie cookie : cookies) {
+                String name = cookie.getName();
                 String token = cookie.getValue();
+                logger.info("auth name : " + name);
                 logger.info("auth cookie : " + token);
-                // if("auth".equals(cookie)) {
+                if(cookie.getName().equals("auth")) {
                     // tp.tokenReader(token);
                     expire = tp.validationToken(token);
                     logger.info("expire check : " + expire);
                     if(!expire) response.setHeader("authcheck", "false");
-                // }
+                }
             }
         } else {
             response.setHeader("authcheck", "false");
             logger.info("expire check : " + expire);
         }
-        return expire;
+        return true;
     }
 }
